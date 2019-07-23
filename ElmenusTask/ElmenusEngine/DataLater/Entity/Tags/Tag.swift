d03030c2fbdd41978f9bea3370b9adac0cc7a4ad
Mohamed Mongy <mohamed.almongy@algorithmz.net>
2019-07-24 01:21:33 +0200

    tags end point

---
+++ ElmenusTask/ElmenusEngine/DataLater/Entity/Tags/Tag.swift
@@ -0,0 +1,15 @@
+//
+//  Tag.swift
+//  ElmenusTask
+//
+//  Created by Mohamed Mongy on 7/24/19.
+//  Copyright © 2019 Mohamed Mongy. All rights reserved.
+//
+
+import Foundation
+
+
+struct Tag {
+    var name: String = ""
+    var photoUrl: String = ""
+}
---
+++ ElmenusTask/ElmenusEngine/DomainLayer/TagsProtocol/TagsProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  TagsProtocol.swift
+//  ElmenusTask
+//
+//  Created by Mohamed Mongy on 7/24/19.
+//  Copyright © 2019 Mohamed Mongy. All rights reserved.
+//
+
+import Foundation
